dd46ccb67e4adb9b9b2991fe55016ac8b2b3057a
Orlandolorite <orlandolorite@gmail.com>
2022-01-16 12:03:34 -0300

    Corrigir html

--- Script.js
+++ Script.js
@@ -1,5 +1,26 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector('button');
+const botao = document.querySelector('botao');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const status = document.querySelector('#status');
+const condicao = document.querySelector('#status');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2` , {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json', 
+            "Content-type": 'application/json'
+        }
+    }).then((Response) => response.json()).then((data) => {
+        imagem.src  = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+
+    });
+
+}
+
+botao.onclick = pegarPersonagem; 
